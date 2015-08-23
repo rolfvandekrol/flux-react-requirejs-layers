14f9db0019ad96dcb3c094ffa34b33b6b9d8254c
Rolf van de Krol <info@rolfvandekrol.nl>
2015-08-23 19:41:12 +0200

    Add some libraries

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var babel = require("gulp-babel");
 var watch = require('gulp-watch');
- 
+var rjs = require('gulp-requirejs');
+var convert = require("gulp-require-convert");
+var rename = require("gulp-rename");
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+
 gulp.task('transform', function () {
-  return gulp.src('js/**/*.jsx')
+  return gulp.src('js/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('build'));
 });
@@ -18,7 +24,31 @@ gulp.task('react', function() {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['transform', 'require', 'react'], function() {
+gulp.task('flux', function() {
+  return gulp.src('node_modules/flux/dist/Flux.js')
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('assign', function() {
+  return gulp.src('node_modules/object-assign/index.js')
+    .pipe(convert())
+    .pipe(rename('assign.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('fbemitter', function() {
+  var b = browserify({
+    entries: 'node_modules/fbemitter/index.js',
+    debug: false,
+    standalone: 'fbemitter'
+  }).bundle();
+
+  return b.pipe(source('index.js'))
+    .pipe(rename('fbemitter.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['transform', 'require', 'react', 'flux', 'assign', 'fbemitter'], function() {
 
 });
 
