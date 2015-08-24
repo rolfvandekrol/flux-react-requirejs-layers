f457c0e8ee4d8072188a9b764668df0556900bea
Rolf van de Krol <info@rolfvandekrol.nl>
2015-08-24 21:49:16 +0200

    Link the plugin component to the store

--- js/components/plugin.js
+++ js/components/plugin.js
@@ -3,14 +3,43 @@
 define(
   ['react', 'stores/plugin_store'],
   function(React, PluginStore) {
+    var getStateFromStore = function(plugin) {
+      return {
+        loaded: PluginStore.loaded(plugin)
+      };
+    };
+
     return React.createClass({
       displayName: 'Plugin',
+
+      propTypes: {
+        plugin: React.PropTypes.string.isRequired
+      },
+
+      getInitialState: function() {
+        return getStateFromStore(this.props.plugin);
+      },
+
       render: function() {
+        var today = new Date();
+        var d = today.toISOString();
         return (
           <div className="plugin">
-            Plugin
+            {d + " " + this.props.plugin + " " + (this.state.loaded ? "loaded" : "unloaded")}
           </div>
         );
+      },
+
+      componentDidMount: function() {
+        PluginStore.addLoadListener(this._onLoad);
+      },
+
+      componentWillUnmount: function() {
+        PluginStore.removeLoadListener(this._onLoad);
+      },
+
+      _onLoad: function() {
+        this.setState(getStateFromStore(this.props.plugin));
       }
     });
   }
