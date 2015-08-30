72863bb259a743bb8abaa9b71de43dcef486bc73
Rolf van de Krol <info@rolfvandekrol.nl>
2015-08-30 13:17:14 +0200

    Move the loading after a props change to the componentDidUpdate method and refactor the action triggering to seperate method

--- js/components/plugin.js
+++ js/components/plugin.js
@@ -41,12 +41,7 @@ define(
       // Link to the PluginStore events to update the state.
       componentDidMount: function() {
         PluginStore.addLoadListener(this._onLoad);
-
-        // Try to load the plugin, if it is not loaded yet. This is only used in
-        // the Direct loading logic.
-        if (!this.state.loaded) {
-          Actions.loadPlugin(this.props.plugin);
-        }
+        this._triggerLoad();
       },
       componentWillUnmount: function() {
         PluginStore.removeLoadListener(this._onLoad);
@@ -55,17 +50,25 @@ define(
       // Regenerate the state when the props change, and run the same logic as
       // in componentWillMount.
       componentWillReceiveProps: function(props) {
-        var new_state = getStateFromStore(props.plugin);
-        this.setState(new_state);
+        this.setState(getStateFromStore(props.plugin));
+      },
 
-        if (!new_state.loaded) {
-          Actions.loadPlugin(props.plugin);
-        }
+      componentDidUpdate: function(prevProps, prevState) {
+        this._triggerLoad();
       },
 
       // Event handler for the PluginStore events.
       _onLoad: function() {
         this.setState(getStateFromStore(this.props.plugin));
+      },
+
+      // Trigger the load Action
+      _triggerLoad: function() {
+        // Try to load the plugin, if it is not loaded yet. This is only used in
+        // the Direct loading logic.
+        if (!this.state.loaded) {
+          Actions.loadPlugin(this.props.plugin);
+        }
       }
     });
   }
